5b9894ead87701d1cdadb960e78a5d1bc68cd7d8
NGM7901 <20210410700059@student.umj.ac.id>
2023-09-13 11:38:54 +0700

    .

--- Assets/cameraC.cs
+++ Assets/cameraC.cs
@@ -2,57 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallCameraManager : MonoBehaviour
+public class CameraSwitcher : MonoBehaviour
 {
-    private Camera tpsCamera; // Kamera TPS
-    private Camera wallCamera; // Kamera dinding
-    private bool nearWall = false; // Status player berdekatan dengan dinding
+    public Camera tpsCamera; // Kamera TPS
+    public Camera fpsCamera; // Kamera FPS
+
+    private bool isFPS = false; // Apakah sedang dalam mode FPS
 
     void Start()
     {
-        // Temukan kamera TPS dan kamera dinding dalam proyek
-        tpsCamera = Camera.main; // Ganti ini dengan kamera TPS Anda jika perlu
-        wallCamera = GameObject.FindGameObjectWithTag("WallCamera").GetComponent<Camera>();
-
-        // Nonaktifkan kamera dinding saat awal
-        wallCamera.enabled = false;
+        // Awalnya, aktifkan kamera TPS dan nonaktifkan kamera FPS
+        tpsCamera.enabled = true;
+        fpsCamera.enabled = false;
     }
 
     void Update()
     {
-        // Mendeteksi apakah player berdekatan dengan dinding dengan raycast dari pemain
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, 2.0f)) // Raycast dimulai dari posisi pemain dan mengikuti arah pandangan pemain
+        // Ubah ukuran dan posisi overlap box sesuai dengan posisi pemain
+        Vector3 boxCenter = transform.position;
+        Vector3 boxSize = new Vector3(2f, 2f, 2f);
+
+        // Pengecekan apakah ada dinding dalam overlap box
+        Collider[] colliders = Physics.OverlapBox(boxCenter, boxSize);
+
+        foreach (Collider collider in colliders)
         {
-            if (hit.collider.CompareTag("Walls")) // Pastikan dinding memiliki tag "Wall"
+            if (collider.CompareTag("Wall")) // Sesuaikan dengan tag dinding Anda
             {
-                nearWall = true;
-
-                // Menampilkan pesan "Tekan E untuk mengganti kamera" jika player dekat dengan dinding
-                // Anda dapat menggunakan GUI atau UI lainnya untuk menampilkan pesan ini sesuai dengan preferensi Anda
-                // Contoh: GUI.Label(new Rect(10, 10, 200, 30), "Tekan E untuk mengganti kamera");
-
-                // Jika pemain menekan tombol "E", beralih kamera
-                if (Input.GetKeyDown(KeyCode.E))
+                // Beralih antara mode FPS dan TPS dengan tombol atau kondisi yang sesuai
+                if (Input.GetKeyDown(KeyCode.E)) // Ganti mode dengan tombol "C"
                 {
-                    SwitchCamera();
+                    isFPS = !isFPS;
+                    tpsCamera.enabled = !isFPS;
+                    fpsCamera.enabled = isFPS;
                 }
             }
-            else
-            {
-                nearWall = false;
-            }
         }
-        else
-        {
-            nearWall = false;
-        }
-    }
-
-    void SwitchCamera()
-    {
-        // Aktifkan kamera TPS dan nonaktifkan kamera dinding jika kamera dinding aktif, dan sebaliknya
-        tpsCamera.enabled = !tpsCamera.enabled;
-        wallCamera.enabled = !wallCamera.enabled;
+        
     }
 }
